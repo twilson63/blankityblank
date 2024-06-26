87d184d3cbeef21d4912cabaa572e1a1eeed190f
Tom Wilson <tom@hyper.io>
2024-06-26 19:11:50 +0000

    update working example

--- index.js
+++ index.js
@@ -2,6 +2,36 @@ import AoLoader from '@permaweb/ao-loader'
 import WeaveDrive from '@permaweb/weavedrive'
 import fs from 'fs'
 
+const FIVE_GB = 5 * 1024 * 1024 * 1024
+const TWO_GB = 2 * 1024 * 1024 * 1024
+
+export function splitArrayBuffer(ab, chunkSize) {
+  const chunks = []
+  const totalBytes = ab.byteLength
+
+  for (let i = 0; i < totalBytes; i += chunkSize) {
+    const end = Math.min(i + chunkSize, totalBytes)
+    chunks.push(ab.slice(i, end))
+  }
+
+  return chunks
+}
+
+export function concatArrayBuffers(abs) {
+  const totalLength = abs.reduce((sum, ab) => sum + ab.byteLength, 0)
+
+  const newGuy = new ArrayBuffer(totalLength)
+  const view = new Uint8Array(newGuy)
+
+  let offset = 0
+  abs.forEach((cur) => {
+    view.set(new Uint8Array(cur), offset)
+    offset += cur.byteLength
+  })
+
+  return newGuy
+}
+
 const wasm = fs.readFileSync('./llama.wasm')
 /**
  * `
@@ -15,6 +45,7 @@ function Init()
 end
 
 Init()
+return "Loaded Llama"
   `
 */
 
@@ -30,7 +61,8 @@ const createMsg = function (data) {
     ],
     Data: data,
     Module: '1234',
-    ['Block-Height']: '1000'
+    ['Block-Height']: '1000',
+    Timestamp: Date.now()
   })
 }
 
@@ -81,8 +113,19 @@ function Init()
 end
 
 Init()
+return "Loaded Llama"
   `), { Process })
-  console.log(result)
+  // console.log(result)
+  console.log('splitting into chunks')
+  const buffArray = splitArrayBuffer(result.Memory, TWO_GB)
+  console.log('combining chunks')
+  const newMemory = concatArrayBuffers(buffArray)
+  console.log('running new memory')
+  const result2 = await handle(newMemory, createMsg(`
+Llama.setPrompt("Tell a Dad Joke")
+return Llama.run(30)
+`), { Process })
+  console.log(result2.Output)
 }
 
 main()
